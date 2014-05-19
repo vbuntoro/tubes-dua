3156479f0ae19527acc38d13189fb68815d1252c
Imballinst <lucent_rays@yahoo.com>
2014-05-19 07:27:23 +0700

    123

--- OOP/src/tubes2/Map.java
+++ OOP/src/tubes2/Map.java
@@ -16,10 +16,7 @@ public class Map {
 		}
 		/* Inisialisasi level 1 */
 		Level = 1;
-		/* Set start node */
-		P[6][3].setStart();
-		/* Set goal node */
-		P[0][3].setGoal();
+		randomMap();
 	}
 	/* Melakukan validasi, 1 adalah depan, 2 adalah belakang, 3 adalah kiri, 4 adalah kanan */
 	public boolean isPathValid(int arah, int i, int j) {
@@ -41,17 +38,34 @@ public class Map {
 	}
 	/* Randomizer map */
 	public void randomMap() {
-		/* Aturan random map, apabila ada path keatas, maka path diatasnya harus ada path 
-		 * kebawah, apabila ada path kekiri, maka path dikirinya harus ada path
-		 * kekanan, dan begitu pula sebaliknya 
-		 * */
-		for (int i=0;i<7;i++) {
-			for (int j=0;j<7;j++) {
-				int x = (int)(Math.random() * 8) + 1;
-				System.out.print(x);
-			}
-			System.out.println();
-		}		
+		/* Melakukan random map dengan 3 pattern
+		 * Setiap pattern map memiliki perbedaan arah dan goal
+		 */
+		int x = (int)(Math.random() * 2) + 1; //random 1-2
+		switch (x) {
+			case 1: P[0][3].setPath(8); P[0][5].setPath(4); P[0][5].setGoal();
+					P[1][3].setPath(4); P[1][4].setPath(8); P[1][5].setPath(4); P[1][6].setPath(8);
+					P[2][2].setPath(8); P[2][3].setPath(5); P[2][4].setPath(1); P[2][5].setPath(3); P[2][6].setPath(2);
+					P[3][1].setPath(8); P[3][2].setPath(8); P[3][4].setPath(4); P[3][6].setPath(4);
+					P[4][1].setPath(7); P[4][2].setPath(1); P[4][3].setPath(4); P[4][4].setPath(3); P[4][5].setPath(4);
+										P[4][6].setPath(2);
+					P[5][2].setPath(4); P[5][4].setPath(2); P[5][5].setPath(4); P[5][6].setPath(6);
+					P[6][2].setPath(4); P[6][2].setStart(); P[6][4].setPath(8);
+					break;
+			case 2: P[0][1].setPath(8); P[0][2].setPath(4); P[0][2].setGoal(); P[0][5].setPath(8); P[0][6].setPath(8);
+					P[1][1].setPath(4); P[1][2].setPath(4); P[1][3].setPath(8); P[1][4].setPath(7); P[1][5].setPath(3);
+										P[1][6].setPath(8);
+					P[2][0].setPath(8); P[2][1].setPath(1); P[2][2].setPath(3); P[2][3].setPath(3); P[2][4].setPath(4);
+										P[2][6].setPath(4);
+					P[3][1].setPath(4); P[3][3].setPath(4); P[3][4].setPath(8); P[3][6].setPath(4);
+					P[4][1].setPath(4); P[4][3].setPath(4); P[4][5].setPath(7); P[4][6].setPath(5);
+					P[5][0].setPath(8); P[5][1].setPath(5); P[5][2].setPath(4); P[5][3].setPath(1); P[5][4].setPath(4);
+										P[5][5].setPath(7); P[5][6].setPath(2);
+					P[6][0].setPath(7); P[6][1].setPath(7); P[6][3].setPath(4); P[6][3].setStart(); P[6][6].setPath(8);
+					break;
+			case 3: break;
+		}
+		
 	}
 	/* Main */
 	public static void main(String args[]) {
